7ed8ccece6442697c6522341e0c098483b610c84
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-10-31 10:47:16 +0900

    Update ex6-9.js

--- ex6-9.js
+++ ex6-9.js
@@ -1,1 +1,3 @@
 function txtchange1(){document.getElementById('txt1').innerHTML='ナンバーワン！';}
+function gazo610(){document.getElementById('gazo610').innerHTML='<img src="./image/14264.png">';}
+function gazo6101(){document.getElementById('gazo6101').inneerHTML='<img src="./image/14261.png">';}
