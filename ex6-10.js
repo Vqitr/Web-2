2061e15aa64187ff0b904f118ed9ac9c31e684cb
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-11-07 10:41:55 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,3 +1,3 @@
 function imgchange1(){document.getElementById('logo').src='./image/999.png';}
 function imgchange2(){document.getElementById('logo').src='./image/998.png';}
-function imgchange3(){document.getElementById('gazou').src='./image/015_l.jpg' class='imagechange';}
+function imgchange3(){document.getElementById('gazou').src='./image/015_l.jpg';}
