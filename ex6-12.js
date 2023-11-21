13fd15b9c0f0f5cde93b60aecd4bccf1fe4fe682
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-11-21 09:55:23 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -5,5 +5,5 @@ if(cnt<11){//現在より１文字多く切り出して表示
   document.timer.moji.value=str.substr(0,cnt+1);}
   else{//すべて表示されたら、空文字に戻す
   document.timer.moji.value="";}}
-function startfncc(){//関数hyoji()を1000ミリ秒間隔で呼び出す
+function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す
 setlnterval("hyoji()",1000);}
