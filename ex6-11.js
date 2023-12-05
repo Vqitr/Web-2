fac12652006509dc44ebc5df2ec3b0b99733244f
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-12-05 10:41:15 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,1 +1,1 @@
-function imgchange1(){document.getElementById('logo2').src='./image/015_l.jpg' class='imagechange';}
+function imgchange1(){document.getElementById('logo2').src='./image/015_l.jpg' class="imagechang";}
