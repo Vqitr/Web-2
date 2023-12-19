48c6ae66edbbdceda8e5358b93523c63fa30b3e8
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-12-19 09:51:35 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo2').src='./image/015_l.jpg';}
-function imgchange2(){document.getElementById('logo2').src='./image/014_l.jpg';}
+function imgchange3(){document.getElementById('logo2').src='./image/015_l.jpg';}
+function imgchange4(){document.getElementById('logo2').src='./image/014_l.jpg';}
