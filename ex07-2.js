509ff91f65eceab7c1c482729fad2de27808d9b6
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-12-19 10:03:52 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,1 +1,4 @@
-function add(){var x=document.getElementById('input1').Value;var y=document.getElementById('input2').value;alert(Number(x)+Number(y));}
+function add(){
+  var x=document.getElementById('input1').Value;
+  var y=document.getElementById('input2').value;
+  alert(Number(x)+Number(y));}
