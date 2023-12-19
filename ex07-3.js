efc09a61e99563286f72f4da525507daed6b8098
Vqitr <131091945+Vqitr@users.noreply.github.com>
2023-12-19 10:01:39 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,2 +1,1 @@
-function bmi(){var x=document.getElementById('taiju').value;var ya-document.getElementById('shincho').value;//1
-alert(x/y^);}
+function bmi(){var x=document.getElementById('taiju').value;var ya-document.getElementById('shincho').value;alert(Number(x)/Number(y)^);}
