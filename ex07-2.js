ca4be506e0ed6f5128e60a00903220a16c2fd30a
Vqitr <131091945+Vqitr@users.noreply.github.com>
2024-01-14 20:42:11 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,4 +1,5 @@
 function add(){
-  var x=document.getElementById('input1').Value;
-  var y=document.getElementById('input2').value;
-  alert(Number(x)+Number(y));}
+  var x = document.getElementById('input1').Value;
+  var y = document.getElementById('input2').value;
+  alert(Number(x)+Number(y));
+}
